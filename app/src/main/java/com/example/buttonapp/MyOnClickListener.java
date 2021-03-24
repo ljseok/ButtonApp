10140a87085a95d5eead2e84300cc888e93a42fb
ccccljs <cho38suk@hamail.net>
2021-03-24 14:46:01 +0900

    added event Listener to a button

---
+++ app/src/main/java/com/example/buttonapp/MyOnClickListener.java
@@ -0,0 +1,17 @@
+package com.example.buttonapp;
+
+import android.view.View;
+
+public class MyOnClickListener implements View.OnClickListener {
+    MainActivity mainActivity;
+    public MyOnClickListener(MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
+    }
+    @Override
+    public void onClick(View v) {
+        mainActivity.mTextView1.setText("You clicked 이준석의 button");
+    }
+
+
+
+}
